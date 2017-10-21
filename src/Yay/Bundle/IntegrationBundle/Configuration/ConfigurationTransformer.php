67847147df6a0d57377ba2be4d02f5a9d983c689
Sven Eisenschmidt <sven.eisenschmidt@gmail.com>
2017-10-21 18:34:26 +0000

    Apply fixes from StyleCI

--- src/Yay/Bundle/IntegrationBundle/Configuration/ConfigurationTransformer.php
+++ src/Yay/Bundle/IntegrationBundle/Configuration/ConfigurationTransformer.php
@@ -3,7 +3,6 @@
 namespace Yay\Bundle\IntegrationBundle\Configuration;
 
 use Symfony\Component\Config\Definition\Processor;
-use Yay\Bundle\IntegrationBundle\Configuration\Configuration;
 use Yay\Component\Entity\Achievement\ActionDefinition;
 use Yay\Component\Entity\Achievement\AchievementDefinition;
 use Yay\Component\Entity\Achievement\Level;
@@ -56,42 +55,42 @@ class ConfigurationTransformer
     {
         $entities = [];
 
-        foreach($processedConfig['actions'] as $name => $action) {
+        foreach ($processedConfig['actions'] as $name => $action) {
             if (!isset($entities[ActionDefinition::class])) {
                 $entities[ActionDefinition::class] = [];
             }
             $entities[ActionDefinition::class][$name] = [
                 '__construct' => [$name],
                 'label' => $action['label'],
-                'description' => $action['description']
+                'description' => $action['description'],
             ];
         }
 
-        foreach($processedConfig['achievements'] as $name => $achievement) {
+        foreach ($processedConfig['achievements'] as $name => $achievement) {
             if (!isset($entities[AchievementDefinition::class])) {
                 $entities[AchievementDefinition::class] = [];
             }
             $entities[AchievementDefinition::class][$name] = [
                 '__construct' => [$name],
                 '__calls' => !empty($achievement['actions'])
-                    ? array_map(function(string $action) {
-                        return ['addActionDefinition' => [ sprintf('@%s', $action) ]];
+                    ? array_map(function (string $action) {
+                        return ['addActionDefinition' => [sprintf('@%s', $action)]];
                     }, $achievement['actions'])
                     : [],
                 'label' => $achievement['label'],
                 'description' => $achievement['description'],
-                'points' => $achievement['points']
+                'points' => $achievement['points'],
             ];
         }
 
-        foreach($processedConfig['levels'] as $name => $level) {
+        foreach ($processedConfig['levels'] as $name => $level) {
             if (!isset($entities[Level::class])) {
                 $entities[Level::class] = [];
             }
             $entities[Level::class][$name] = [
                 '__construct' => [$name, $level['level'], $level['points']],
                 'label' => $level['label'],
-                'description' => $level['description']
+                'description' => $level['description'],
             ];
         }
 
@@ -107,18 +106,18 @@ class ConfigurationTransformer
     {
         $services = [
             'services' => [
-                '_defaults' => ['autoconfigure' => true]
-            ]
+                '_defaults' => ['autoconfigure' => true],
+            ],
         ];
 
-        foreach($processedConfig['validators'] as $name => $validator) {
-            if($validator['type'] === 'expression') {
+        foreach ($processedConfig['validators'] as $name => $validator) {
+            if ($validator['type'] === 'expression') {
                 $validator['class'] = ExpressionLanguageValidator::class;
             }
 
             $services['services'][$name] = [
                 'class' => $validator['class'],
-                'arguments' => $validator['arguments']
+                'arguments' => $validator['arguments'],
             ];
         }
 
