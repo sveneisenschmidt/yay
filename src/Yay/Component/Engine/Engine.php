a20a3c2b658891e7c40b246944e8cdc74540103a
Sven Eisenschmidt <sven.eisenschmidt@gmail.com>
2017-10-29 22:53:54 +0100

    Remove unused indexes

--- src/Yay/Component/Engine/Engine.php
+++ src/Yay/Component/Engine/Engine.php
@@ -166,20 +166,19 @@ class Engine
     {
         // Collect players to refresh them later
         $players = [];
-        foreach ($collection as $index => $personalAction) {
+        foreach ($collection as $personalAction) {
             if (!in_array($personalAction->getPlayer(), $players)) {
                 $players[] = $personalAction->getPlayer();
             }
         }
 
         // Persist new personalActions to database
-        /** @var PersonalActionInterface $personalAction */
-        foreach ($collection as $index => $personalAction) {
+        foreach ($collection as $personalAction) {
             $this->savePersonalAction($personalAction);
         }
 
         // Refresh players
-        foreach ($players as $index => $player) {
+        foreach ($players as $player) {
             $this->refreshPlayer($player);
         }
     }
