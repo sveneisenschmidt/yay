d0fd088ae32a10e40dcbca13b786e92ec96609c7
Sven Eisenschmidt <sven.eisenschmidt@trivago.com>
2017-11-02 19:28:26 +0100

    Added testcases

--- src/Yay/Bundle/ApiBundle/Tests/Controller/ProgressControllerTest.php
+++ src/Yay/Bundle/ApiBundle/Tests/Controller/ProgressControllerTest.php
@@ -8,7 +8,29 @@ class ProgressControllerTest extends WebTestCase
      * @test
      * @testdox Submit a payload to update a users progress
      */
-    public function Progress_SubmitAction_ValidUser_OneAction()
+    public function Progress_SubmitGetAction_ValidUser_OneAction()
+    {
+        $client = static::createClient();
+
+        $content = http_build_query([
+            'player' => 'jane.doe',
+            'action' => 'yay.action.test_action',
+        ]);
+
+        $client->request('GET', sprintf('/api/progress/?%s', $content));
+        $response = $client->getResponse();
+
+        $this->assertTrue($response->isOk());
+        $this->assertJson($content = $response->getContent());
+        $this->assertInternalType('array', $data = json_decode($content, true));
+        $this->assertEmpty($data);
+    }
+
+    /**
+     * @test
+     * @testdox Submit a payload to update a users progress
+     */
+    public function Progress_SubmitPostAction_ValidUser_OneAction()
     {
         $client = static::createClient();
 
@@ -25,12 +47,65 @@ class ProgressControllerTest extends WebTestCase
         $this->assertInternalType('array', $data = json_decode($content, true));
         $this->assertEmpty($data);
     }
+    /**
+     * @test
+     * @testdox Submit a payload to update a users progress
+     */
+    public function Progress_SubmitGetAction_ValidUser_ManyAction()
+    {
+        $client = static::createClient();
+
+        $content = http_build_query([
+            'player' => 'jane.doe',
+            'actions' => [
+                'yay.action.test_action',
+                'yay.action.test_action',
+                'yay.action.test_action',
+                'yay.action.test_action',
+                'yay.action.test_action',
+            ],
+        ]);
+
+        // (1) Push new actions
+        $client->request('GET', sprintf('/api/progress/?%s', $content));
+        $response = $client->getResponse();
+
+        $this->assertTrue($response->isOk());
+        $this->assertJson($content = $response->getContent());
+        $this->assertInternalType('array', $data = json_decode($content, true));
+        $this->assertNotEmpty($data);
+
+        foreach ($data as $key => $value) {
+            $this->assertArrayHasKey('name', $value);
+            $this->assertArrayHasKey('achieved_at', $value);
+            $this->assertArraySubsetHasKey('links', 'self', $value);
+            $this->assertArraySubsetHasKey('links', 'player', $value);
+            $this->assertArraySubsetHasKey('links', 'achievement', $value);
+        }
+
+        // (2) Get a player's achievements
+        $client->request('GET', '/api/players/jane.doe/personal-achievements/');
+        $response = $client->getResponse();
+
+        $this->assertTrue($response->isOk());
+        $this->assertJson($content = $response->getContent());
+        $this->assertInternalType('array', $data = json_decode($content, true));
+        $this->assertNotEmpty($data);
+
+        foreach ($data as $key => $value) {
+            $this->assertArrayHasKey('name', $value);
+            $this->assertArrayHasKey('achieved_at', $value);
+            $this->assertArraySubsetHasKey('links', 'self', $value);
+            $this->assertArraySubsetHasKey('links', 'player', $value);
+            $this->assertArraySubsetHasKey('links', 'achievement', $value);
+        }
+    }
 
     /**
      * @test
      * @testdox Submit a payload to update a users progress
      */
-    public function Progress_SubmitAction_ValidUser_ManyAction()
+    public function Progress_SubmitPostAction_ValidUser_ManyAction()
     {
         $client = static::createClient();
 
@@ -80,11 +155,30 @@ class ProgressControllerTest extends WebTestCase
         }
     }
 
+
     /**
      * @test
      * @testdox Submit a payload, but missing actions, to update a users progress
      */
-    public function Progress_SubmitAction_ValidUser_NoAction()
+    public function Progress_SubmitGetAction_ValidUser_NoAction()
+    {
+        $client = static::createClient();
+
+        $content = http_build_query([
+            'player' => 'jane.doe',
+        ]);
+
+        $client->request('GET', sprintf('/api/progress/?%s', $content));
+        $response = $client->getResponse();
+
+        $this->assertTrue($response->isNotFound());
+    }
+
+    /**
+     * @test
+     * @testdox Submit a payload, but missing actions, to update a users progress
+     */
+    public function Progress_SubmitPostAction_ValidUser_NoAction()
     {
         $client = static::createClient();
 
@@ -102,7 +196,26 @@ class ProgressControllerTest extends WebTestCase
      * @test
      * @testdox Submit a payload, but unkown player, to update a users progress
      */
-    public function Progress_SubmitAction_ValidUser_UnknownPlayer()
+    public function Progress_SubmitGetAction_ValidUser_UnknownPlayer()
+    {
+        $client = static::createClient();
+
+        $content = http_build_query([
+            'player' => 'john.doe',
+            'action' => 'yay.action.test_action',
+        ]);
+
+        $client->request('GET', sprintf('/api/progress/?%s', $content));
+        $response = $client->getResponse();
+
+        $this->assertTrue($response->isNotFound());
+    }
+
+    /**
+     * @test
+     * @testdox Submit a payload, but unkown player, to update a users progress
+     */
+    public function Progress_SubmitPostAction_ValidUser_UnknownPlayer()
     {
         $client = static::createClient();
 
@@ -121,7 +234,29 @@ class ProgressControllerTest extends WebTestCase
      * @test
      * @testdox Submit a payload, but missing actions, to update a users progress
      */
-    public function Progress_SubmitAction_ValidUser_UnknownAction()
+    public function Progress_SubmitGetAction_ValidUser_UnknownAction()
+    {
+        $client = static::createClient();
+
+        $content = http_build_query([
+            'player' => 'jane.doe',
+            'action' => 'yay.action.unknown_test_action',
+        ]);
+
+        $client->request('GET', sprintf('/api/progress/?%s', $content));
+        $response = $client->getResponse();
+
+        $this->assertTrue($response->isOk());
+        $this->assertJson($content = $response->getContent());
+        $this->assertInternalType('array', $data = json_decode($content, true));
+        $this->assertEmpty($data);
+    }
+
+    /**
+     * @test
+     * @testdox Submit a payload, but missing actions, to update a users progress
+     */
+    public function Progress_SubmitPostAction_ValidUser_UnknownAction()
     {
         $client = static::createClient();
 
