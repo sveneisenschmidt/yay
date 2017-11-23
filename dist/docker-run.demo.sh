5e1cf5009cdaf9f0824646fba9eeedf39d5e43c8
Sven Eisenschmidt <sveneisenschmidt@users.noreply.github.com>
2017-11-23 13:58:07 +0100

    Use umask to handle file permissions (Closes #106)

--- dist/docker-run.demo.sh
+++ dist/docker-run.demo.sh
@@ -2,6 +2,7 @@
 
 if [ "${APP_MODE}" == "install" ]
 then
+    php bin/console cache:warmup  --env=${APP_ENV};
     php bin/console doctrine:schema:drop --force --em=default --env=${APP_ENV};
     php bin/console doctrine:schema:create --em=default --env=${APP_ENV};
     php bin/console yay:integration:enable default integration/default --env=${APP_ENV};
@@ -11,6 +12,7 @@ fi
 
 if [ "${APP_MODE}" == "web" ]
 then
+    php bin/console cache:warmup  --env=${APP_ENV};
     php bin/console yay:integration:enable default integration/default --config-only --env=${APP_ENV};
     php bin/console yay:integration:enable demo integration/demo --config-only --env=${APP_ENV};
     apache2-foreground
