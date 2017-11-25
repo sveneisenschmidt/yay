18bdf5a700c12abb566e94ebf32d7f60b0590f74
Sven Eisenschmidt <sven.eisenschmidt@gmail.com>
2017-11-25 11:40:37 +0100

    Enable build step

--- makedefs/publish.mk
+++ makedefs/publish.mk
@@ -6,8 +6,8 @@ define .publish
 	rm -rf var/* config/integration/*
 	cp $(1) docker-run.sh
 	chmod +x docker-run.sh
-#	docker build --squash --compress -t "$(2):$(3)" .
-#	docker push "$(2):$(3)"
+	docker build --squash --compress -t "$(2):$(3)" .
+	docker push "$(2):$(3)"
 	if [ "$(3)" = "stable" ]; then docker tag "$(2):$(3)" "$(2):latest"; fi
 	if [ "$(3)" = "stable" ]; then docker push "$(2):latest"; fi
 	if [ "$(3)" = "dev" ]; then docker tag "$(2):$(3)" "$(2):dev-$(4)"; fi
