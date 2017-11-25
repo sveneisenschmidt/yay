4d96032166c71beba4bbbf233b8e9b9d253b52cd
Sven Eisenschmidt <sven.eisenschmidt@gmail.com>
2017-11-25 11:31:27 +0100

    Docker test

--- makedefs/publish.mk
+++ makedefs/publish.mk
@@ -6,9 +6,11 @@ define .publish
 	rm -rf var/* config/integration/*
 	cp $(1) docker-run.sh
 	chmod +x docker-run.sh
-	docker build --squash --compress -t "$(2):$(3)" .
-	docker push "$(2):$(3)"
-	if [[ "$(3)" == "stable" ]]; then docker tag "$(2):$(3)" "$(2):latest" && docker push "$(2):latest"; fi
-	if [[ "$(3)" == "dev" ]]; then docker tag "$(2):$(3)" "$(2):dev-$(4)" && docker push "$(2):dev-$(4)"; fi
+#	docker build --squash --compress -t "$(2):$(3)" .
+#	docker push "$(2):$(3)"
+#	if [ "$(3)" = "stable" ]; then docker tag "$(2):$(3)" "$(2):latest" && docker push "$(2):latest"; fi
+#	if [ "$(3)" = "dev" ]; then docker tag "$(2):$(3)" "$(2):dev-$(4)" && docker push "$(2):dev-$(4)"; fi
+	if [ "dev1" = "dev1" ]; then echo "test1"; fi
+	if [ "dev1" = "dev2" ]; then echo "test2"; fi
 	rm docker-run.sh
 endef
