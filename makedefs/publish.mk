7394382b2ff1e18362be9829b0d541c54b0dc067
Sven Eisenschmidt <sven.eisenschmidt@gmail.com>
2017-11-25 11:17:48 +0100

    Different bash comparission

--- makedefs/publish.mk
+++ makedefs/publish.mk
@@ -8,7 +8,7 @@ define .publish
 	chmod +x docker-run.sh
 	docker build --squash --compress -t "$(2):$(3)" .
 	docker push "$(2):$(3)"
-	if [ "$(3)" == "stable" ]; then docker tag "$(2):$(3)" "$(2):latest" && docker push "$(2):latest"; fi
-	if [ "$(3)" == "dev" ]; then docker tag "$(2):$(3)" "$(2):dev-$(4)" && docker push "$(2):dev-$(4)"; fi
+	if [[ "$(3)" == "stable" ]]; then docker tag "$(2):$(3)" "$(2):latest" && docker push "$(2):latest"; fi
+	if [[ "$(3)" == "dev" ]]; then docker tag "$(2):$(3)" "$(2):dev-$(4)" && docker push "$(2):dev-$(4)"; fi
 	rm docker-run.sh
 endef
