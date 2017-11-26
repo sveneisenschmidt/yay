f2f3f74634dc3eb4917ece7c871858a662af9b3f
Sven Eisenschmidt <sveneisenschmidt@users.noreply.github.com>
2017-11-26 14:28:05 +0100

    Apply fixes from StyleCI (#116)

--- src/App/Engine/Engine.php
+++ src/App/Engine/Engine.php
@@ -7,18 +7,16 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 use App\Engine\DependencyInjection\AchievementValidatorPass;
 use App\Engine\DependencyInjection\EngineExtension;
 use Component\Engine\AchievementValidatorInterface;
-use Component\Engine\EventListener\EventListenerInterface;
 use Symfony\Component\EventDispatcher\DependencyInjection\RegisterListenersPass;
 
 class Engine extends Bundle
 {
     public function build(ContainerBuilder $container)
     {
-        $container->addCompilerPass(new RegisterListenersPass('event_dispatcher', 'yay.event_listener', 'yay.event_subscriber'));        
+        $container->addCompilerPass(new RegisterListenersPass('event_dispatcher', 'yay.event_listener', 'yay.event_subscriber'));
         $container->addCompilerPass(new AchievementValidatorPass());
         $container->registerForAutoconfiguration(AchievementValidatorInterface::class)
             ->addTag('yay.achievement_validator');
-
     }
 
     public function getContainerExtension(): EngineExtension
