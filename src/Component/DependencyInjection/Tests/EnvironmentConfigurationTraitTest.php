ee95700dfe188d055d15a3a6bda31fe8630a06a8
Sven Eisenschmidt <sven.eisenschmidt@gmail.com>
2017-12-12 13:27:58 +0100

    Unified the service loading in bundles through a common trait

---
+++ src/Component/DependencyInjection/Tests/EnvironmentConfigurationTraitTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Component\DependencyInjection\tTests;
+
+use PHPUnit\Framework\TestCase;
+use Component\DependencyInjection\EnvironmentConfigurationTrait;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class EnvironmentConfigurationTraitTest extends TestCase
+{
+    public function createInstance(): object {
+        return new class() {
+            use EnvironmentConfigurationTrait;
+        };
+    }
+
+    public function test_load_from_directory()
+    {
+        $configs = [];
+        $container = $this->createConfiguredMock(ContainerBuilder::class, [
+            'getParameter' => 'test'
+        ]);
+
+        $instance = $this->createInstance();
+        $loaded = $instance->loadFromDirectory(
+            __DIR__.'/Fixtures/EnvironmentConfigurationTrait', 
+            $configs, 
+            $container
+        );
+
+        $this->assertEquals(2, $loaded);
+    }
+
+    public function test_load_from_directory_but_different_environment()
+    {
+        $configs = [];
+        $container = $this->createConfiguredMock(ContainerBuilder::class, [
+            'getParameter' => 'dev'
+        ]);
+
+        $instance = $this->createInstance();
+        $loaded = $instance->loadFromDirectory(
+            __DIR__.'/Fixtures/EnvironmentConfigurationTrait', 
+            $configs, 
+            $container
+        );
+
+        $this->assertEquals(1, $loaded);
+    }
+}
