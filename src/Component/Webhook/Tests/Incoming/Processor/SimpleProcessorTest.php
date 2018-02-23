b19200db3228c25d8e9cbed86fca063fe5be18e6
Sven Eisenschmidt <sven.eisenschmidt@gmail.com>
2018-02-23 16:03:00 +0100

    Added tests and improved documentation

--- src/Component/Webhook/Tests/Incoming/Processor/SimpleProcessorTest.php
+++ src/Component/Webhook/Tests/Incoming/Processor/SimpleProcessorTest.php
@@ -9,30 +9,57 @@ use Component\Webhook\Incoming\Processor\SimpleProcessor;
 
 class SimpleProcessorTest extends TestCase
 {
+    public function providePayloads()
+    {
+        return [
+            [
+                file_get_contents(__DIR__.'/Fixtures/SimplePushAction.json'),
+                'Alex Doe',
+                'simple.push',
+            ],
+            [
+                file_get_contents(__DIR__.'/Fixtures/SimplePullAction.json'),
+                'Alex Doe',
+                'simple.pull',
+            ],
+        ];
+    }
+
     public function test_set_get_name(): void
     {
         $processor = new SimpleProcessor('test');
         $this->assertEquals('test', $processor->getName());
     }
 
-    public function test_process_payload(): void
-    {
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-          );
+    /** @dataProvider providePayloads */
+    public function test_process_payload(
+        string $contents,
+        string $username,
+        string $action
+    ): void {
+        $request = Request::create('/', 'POST', [], [], [], [], $contents);
+
+        (new SimpleProcessor('simple-processor'))->process($request);
+        $this->assertEquals($username, $request->request->get('username'));
+        $this->assertEquals($action, $request->request->get('action'));
     }
 
+    /** @expectedException InvalidArgumentException */
     public function test_broken_payload(): void
     {
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-          );
+        $contents = ',1%}';
+        $request = Request::create('/', 'POST', [], [], [], [], $contents);
+
+        (new SimpleProcessor('simple-processor'))->process($request);
     }
 
     public function test_empty_payload(): void
     {
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-          );
+        $contents = json_encode([]);
+        $request = Request::create('/', 'POST', [], [], [], [], $contents);
+
+        (new SimpleProcessor('simple-processor'))->process($request);
+        $this->assertFalse($request->request->has('username'));
+        $this->assertFalse($request->request->has('action'));
     }
 }
