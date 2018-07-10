465a18856f07c437bbe5910f8542ad483dbabcdd
Sven Eisenschmidt <sven.eisenschmidt@gmail.com>
2018-07-10 19:02:18 +0200

    Make api doc routing compatible

--- src/App/Compat/Compat.php
+++ src/App/Compat/Compat.php
@@ -5,8 +5,6 @@ namespace App\Compat;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use App\Compat\DependencyInjection\CompatExtension;
-use App\Compat\DependencyInjection\JMSSerializerBundlePass;
-use App\Compat\DependencyInjection\SncRedisBundlePass;
 use App\Compat\DependencyInjection\NelmioApiDocBundlePass;
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 
@@ -14,8 +12,6 @@ class Compat extends Bundle
 {
     public function build(ContainerBuilder $container)
     {
-        // $container->addCompilerPass(new JMSSerializerBundlePass(), PassConfig::TYPE_AFTER_REMOVING, -255);
-        // $container->addCompilerPass(new SncRedisBundlePass(), PassConfig::TYPE_AFTER_REMOVING, -255);
         $container->addCompilerPass(new NelmioApiDocBundlePass(), PassConfig::TYPE_AFTER_REMOVING, -255);
     }
 
